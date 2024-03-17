3f38d984bd136caebc52223fe55109fd22bb036c
nabielascar <ascarnabiel@gmail.com>
2024-03-18 03:01:20 +0700

    first commit

---
+++ TPMODUL5_1302220080/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TPMODUL5_1302220080
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
