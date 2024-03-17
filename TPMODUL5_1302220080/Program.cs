9ab91eae1219a1d4c8d43d84fe91da7dc730e24a
nabielascar <ascarnabiel@gmail.com>
2024-03-18 03:08:47 +0700

    menambahkan class DataGeneric

--- TPMODUL5_1302220080/Program.cs
+++ TPMODUL5_1302220080/Program.cs
@@ -6,24 +6,29 @@ using System.Threading.Tasks;
 
 namespace TPMODUL5_1302220080
 {
-    public class HaloGeneric
+    class DataGeneric<T>
     {
-        public void SapaUser<T>(T userName)
+        T Data { get; set; }
+        public DataGeneric(T data)
         {
-            Console.WriteLine($"Halo user {userName}");
+            Data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {Data}");
         }
     }
-
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            HaloGeneric halo = new HaloGeneric();
-
-
-            string namaPanggilan = "Ascar";
-            halo.SapaUser(namaPanggilan);
-            Console.Read();
+            DataGeneric<string> stringData = new DataGeneric<string>("Y");
+            stringData.PrintData();
+            DataGeneric<string> nimData = new DataGeneric<string>("1302220080");
+            nimData.PrintData();
+            Console.ReadLine();
         }
     }
 }
